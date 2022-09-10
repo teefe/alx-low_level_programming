48b7fecd82f0a899bb3d3cf49387da62456b3f87
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-10 13:30:29 +0300

    Removed a print line

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,13 +11,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%d ", n);
 	if (n > 0)
-		printf("is positive");
+		printf("%d is positive", n);
 	if (n < 0)
-		printf("is negative");
+		printf("%d is negative", n);
 	if (n == 0)
-		printf("is zero");
+		printf("%d is zero", n);
 	return (0);
 }
 
