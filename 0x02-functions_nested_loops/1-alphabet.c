aacc5f1c1338fd437de8151b0d6a11bfbd701e7c
Katabira Derrick Teefe <derrickteefe20@@gmail.com>
2022-09-13 16:29:23 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,19 +4,19 @@
 /**
  * main - Prints the alpharbet using the putchar function
  *
- * print_1-alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  *
  */
-void print_1-alphabet(void)
+void print_alphabet(void)
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 }
-	putchar('\n')
+	putchar('\n');
 }
 int main(void)
 {
