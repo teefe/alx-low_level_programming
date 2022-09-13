734499bfe0a5a1ff1539be1e2b730ff90cc58ba6
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-14 01:51:21 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+  
+void print_putchar(void)
+{
+	int counter = 0;
+
+	int chars[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	while (counter < 8)
+	{
+		putchar(chars[counter]);
+		counter++;
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_putchar();
+	return (0);
+}
