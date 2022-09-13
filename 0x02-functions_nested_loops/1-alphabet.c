cd93a19bca3eae4d9fcedb31c21a33fb5d4dc28d
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-14 02:33:53 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - Prints the alpharbet using the putchar function
- *
  * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
- *
  */
 void print_alphabet(void)
 {
-	int ch;
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
-	putchar('\n')
