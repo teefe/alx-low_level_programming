3bc319fc0aa95839fe44b4ea2d671b44b017c9a3
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-19 23:27:33 +0300

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,11 +4,10 @@
 *1-swap.c - swpping the int values
 *@a: int swap variable
 *@b: another swap variable
-*@c: temp swap variable
 *
 *Return: Always 0
 */
-void 1-swap(int *a,int *b)
+void 1_swap(int *a, int *b)
 {
 	int c = *a;
 	*a = *b;
