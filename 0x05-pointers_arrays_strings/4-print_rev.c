dcd2f32170db9a8b8fbb4cb72ee054e63eae4894
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-20 01:18:25 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+*_strlen - counts the string length
+*@s:return value
+*
+*Return: string length
+*/
+int _strlen(char s[])
+{
+	int r = 0;
+	{
+	while (s[r] != '\0')
+		r++;
+	return (r);
+	}
+}
+
+/**
+*print_rev - print rev string
+*@s:param string
+*
+*Return: Always 0
+*/
+void print_rev(char s[])
+{
+	int r = _strlen(s);
+
+	while (r >= 0)
+	{
+		putchar(s[r]);
+		r++;
+	}
+	printf("\n");
+}
