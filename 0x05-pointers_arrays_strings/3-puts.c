e9ec4ec28f127cbe02f3fe4d06e89173ed0b27b0
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-20 00:50:39 +0300

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_puts - print string
+*@s:param string
+*
+*Return: Always 0
+*/
+void _puts(char s[])
+{
+	int r = 0;
+
+	while (s[r] != '\0')
+	{
+		putchar(s[r])
+		r++;
+	}
+}
