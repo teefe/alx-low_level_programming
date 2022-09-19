c0295d8975f745cac5755e986d38ba9cb5baf437
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-20 01:59:16 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,16 +8,16 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-char *_strcpy(char d[], char src[])
+char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 
 	while (count >= 0)
 	{
-		d[] = src[];
-		if (src[] == '\0')
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
 			break;
 		count++;
 	}
-	return (d);
+	return (dest);
 }
