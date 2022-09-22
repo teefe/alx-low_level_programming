0270aaa21c41244240ce6fd3913d022e4138777c
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-22 22:07:56 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,32 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * 
- * 
- * 
- */
+*_strcat - concatenates two strings,
+*@dest: destination.
+*@src: source.
+*Return: the pointer to dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i = 0;
 
-char greetings[] = "Hello World!";
+	while (*(dest + destlen) != '\0')
+	{
+		destlen++;
+	}
 
-printf("%s", greetings);
+	while (*(src + srclen) != '\0')
+	{
+		srclen++;
+	}
+
+	while (i < srclen)
+	{
+	*(dest + destlen) = *(src + i);
+	i++;
+	destlen++;
+	}
+	return (dest);
+}
