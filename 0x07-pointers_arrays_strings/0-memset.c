ba781dd7740842371bf6e543cbd8f39c8968e84f
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-27 00:08:00 +0300

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _memset - Fill memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to fill
- * @n: bytes of memory area to fill
- *
- * Return: the memory area filled
- */
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
-	a = 0;
-
-	while (a < n)
+	for (a = 0; a < n; a++)
 	{
 		*(s + a) = b;
-		a++;
 	}
+
 	return (s);
 }
