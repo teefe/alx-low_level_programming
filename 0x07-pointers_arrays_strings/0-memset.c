d9db217a4ba9bfd9ed89093f699806acd6c1b9e7
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-27 00:01:35 +0300

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - Fill memory with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memory area to fill
+ *
+ * Return: the memory area filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	a = 0;
+
+	while (a < n)
+	{
+		*(s + a) = b;
+		a++;
+	}
+	return (s);
+}
