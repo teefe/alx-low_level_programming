169ef6d5c0641937264f388f53b5e6f71e8c7700
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-29 00:28:22 +0300

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
-* factorial - factorial function
-* @n: the string
+*factorial - factorial function
+*@n: the string
 *
-* return:the factorial of n
+*Return: the factorial of @n
 */
 int factorial(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
 	if (n == 0)
 	{
 		return (1);
