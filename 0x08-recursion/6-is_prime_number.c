38939e3ac6c6744efce10098d3f08ea396ea393a
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-09-29 03:06:57 +0300

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#inlcude "main.h"
+
+/**
+ * primeFind - the find sqrt function
+ * @b: the guess number
+ * @p: the prime number
+ * Return: wether number is prime
+ */
+int primeFind(int p, int b)
+{
+	if (p <= 1)
+		return (0);
+	if (p % b == 0 && b > 1)
+		return (0);
+	if (p / b < b)
+		return (1);
+	return (primeFind(p, b + 1));
+}
+/**
+ * is_prime_number - the sqrt recursion function
+ * @n: the number to be checked
+ *Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	return (primeFind(n, 2));
+}
+
