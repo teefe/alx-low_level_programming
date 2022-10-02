2ea90219b44a342b3d6ecd2b245d6da933a4b169
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-10-03 02:16:07 +0300

    ./nargs hello, world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main-print sthe number of args
+*
+*@argc:arguemnt count
+*@argv: argument vector
+*
+*Return:0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
