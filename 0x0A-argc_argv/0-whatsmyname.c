10a0a07fdeef4cfa821e043d054f0552147ba7e5
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-10-03 00:15:21 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main-function to print its name
+*
+*@argc: argument count
+*@argv: argument vector
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+}
